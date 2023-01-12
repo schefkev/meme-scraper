d2bf076618045c387e05abde28503230f5254852
Kevin Toth <kevin.toth16@gmail.com>
2023-01-12 22:17:13 +0100

    Add memes to gitignore

--- README.md
+++ README.md
@@ -1,28 +1,11 @@
-reate a cli (Command Line Interface) application that scrapes the current version of this website:
+[![Run on Repl.it](https://replit.com/@schefkev/meme-scraper#memes/03.jpg)]
 
-https://memegen-link-examples-upleveled.netlify.app/
+# Node.js Meme Scraper
 
-...and saves the first 10 images into a folder called "memes" within the directory of the new project. The image files should be named with a number with a leading zero, eg. 01.jpg, 02.jpg, etc.
+Scrape the first 10 images from https://memegen-link-examples-upleveled.netlify.app/ and save it to the `memes` folder.
 
-Avoid using an "image scraper" or "image downloader" library that does multiple steps at once for you (eg. do not use image-downloader or nodejs-file-downloader or similar) - break the task down into smaller steps and select libraries as necessary for each step.
+## Hot to run this precious little thing
 
-Make sure that the meme images are "ignored" in Git - they should not show up in your repository.
+to get the meme images from the website, write the following on the command line
 
-The program should be able to run multiple times without throwing an error.
-
-TODOs
-Create a readme
-Add memes to the .gitignore
-x Set a variable with the URL to the website
-x Request HTML information from the URL (maybe "fetch get request")
-x Save this HTML into a variable called htmlContent (string)
-Inside htmlContent:
-look for all matching elements (maybe an <a> element that contains an image or maybe an <img />)
-extract the img src attribute (string)
-assign this array of strings to a new variable called imgSrcs / links / imageUrls
-Filter the array to create a new array of only 10 elements
-Make a folder named memes
-Loop through each URL in the image urls and
-Request image data (jpg data)
-Save image data to a new file named 01.jpg, 02.jpg, etc in the memes folder
-Maybe at the beginning, before each of run of the program, we want to also clean the folder so that there are no conflicts?
+`node index.js`
--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 import fs from 'node:fs';
 import client from 'node:https';
-// import path from 'node:path';
 import axios from 'axios';
 import * as cheerio from 'cheerio';
+import fsExtra from 'fs-extra';
 
 const url = 'https://memegen-link-examples-upleveled.netlify.app/';
 const filePath = './memes';
 
-// Making directory which is empty
+// First, this will check if the directory exist, if it does exist, it will clean out
+// the content. If it does not exist, it will mkdir
 try {
   if (fs.existsSync(filePath)) {
-    console.log('Directory already exists.');
+    fsExtra.emptyDirSync(filePath);
+    console.log('Directory successfully cleaned-up');
   } else {
-    fs.mkdir(filePath, { recursive: true }, (err) => {
-      if (err) throw err;
+    fs.mkdir(filePath, { recursive: true }, () => {
       console.log('Directory successfully created!');
     });
   }
@@ -26,19 +27,24 @@ async function scrapeData() {
   try {
     // Fetch HTML of the page we want to scrape
     const { data } = await axios.get(url);
+
     // Load HTML we fetched in the previous line
     const htmlContent = cheerio.load(data);
-    // Select all the list items in plainlist class
-    const imgItems = htmlContent('div a img');
-    // Stores data for all countries
+    console.log('Website Online, starting the scraping process now!');
+
+    // Select all the img items
+    const imgItems = htmlContent('img');
+
+    // Stores data for all images
     const linkArray = [];
-    // Use .each method to loop through the li we selected
+
+    // Use .each method to loop through the htmlContent we selected
     imgItems.each((idx, el) => {
       if (idx <= 9) {
         const link = htmlContent(el).attr('src');
         linkArray.push(link);
       }
-
+      // Looping over the linkArray.length
       for (let i = 0; i < linkArray.length; i++) {
         client.get(linkArray[i], (res) => {
           const dir = `./memes/0${i + 1}.jpg`;
@@ -46,21 +52,9 @@ async function scrapeData() {
         });
       }
     });
-    // Logs linkArray to the console
-    console.dir(linkArray);
   } catch (err) {
     console.error(err);
   }
 }
 // Invoke the above function
 await scrapeData();
-
-// Removing directory which is not empty!
-// setTimeout(() => {
-//   fs.rmSync(filePath, { recursive: true, force: true }, (err) => {
-//     if (err) {
-//       console.log('error occurred in deleting directory', err);
-//     }
-//     console.log('Directory deleted successfully');
-//   });
-// }, '10000');
